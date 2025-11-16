af7823cb629ac1c5c86b0bf6701fa2e59b3ec975
hansgeorg775 <hansgeorg775@gmail.com>
2025-11-16 17:55:56 +0100

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,7 +12,7 @@ function LoginForm() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/login", {
+    const response = await fetch("https://backend-wvgi.onrender.com/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ user, password })
